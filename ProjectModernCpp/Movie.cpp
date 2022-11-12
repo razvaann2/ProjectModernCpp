c47be00cc1a4383bd0cb14b3238649f26eb782cd
Horia <senduchoria@yahoo.com>
2022-11-12 19:00:37 +0200

    Movie Constructor

---
+++ ProjectModernCpp/Movie.cpp
@@ -0,0 +1,22 @@
+#include "Movie.h"
+
+Movie::Movie()
+{
+	MovieId = "";
+	Type = "";
+	Title = "";
+	Director = "";
+	Cast = "";
+	Country = "";
+	DateAdded = "";
+	ReleaseYear = "";
+	Rating = "";
+	Duration = "";
+	ListedIn = "";
+	Description = "";
+}
+
+Movie::~Movie()
+{
+
+}
