b96b1ddcb9c604fce67cd84979673e35ba0e4a6a
Olteanu Razvan <razvaann2@gmail.com>
2022-11-12 16:47:15 +0200

    UserHeader

---
+++ ProjectModernCpp/User.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <QString>
+#include <vector>
+class User
+{
+private:
+	QString userName;
+	std::vector<int>MoviesWatched;
+	std::vector<int>WishList;
+public:
+	User();
+	~User();
+	User(User& x);
+	void SetUserName(QString userName);
+	QString SetUserName();
+};
