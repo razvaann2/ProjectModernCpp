33854a61e8de430daea3e3293928a8c19311f64b
Olteanu Razvan <razvaann2@gmail.com>
2022-11-25 04:07:26 +0200

    DataBase_update

--- ProjectModernCpp/DataBase.h
+++ ProjectModernCpp/DataBase.h
@@ -1,8 +1,24 @@
 #pragma once
-#include "InitializeStorage.h"
+#include <sqlite_orm/sqlite_orm.h>
+#include "User.h"
+namespace sql = sqlite_orm;
 
+const std::string dbFile = "DataBase.sqlite";
+inline auto createStorage(const std::string& filename)
+{
+	return sql::make_storage(
+		filename,
+		sql::make_table("User",
+			sql::make_column("UserID", &User::GetID, &User::SetID, sql::autoincrement(), sql::primary_key()),
+			sql::make_column("UserName", &User::GetUserName, &User::SetUserName))
+
+		);
+}
+using Storage = decltype(createStorage(" "));
+//std::string Type, Title, Director, Cast, Country, DateAdded, ReleaseYear, Rating, Duration, ListedIn, Description;
 class DataBase
 {
 public:
-	Storage database = initStorage("storage.sqlite");
+	Storage m_db = createStorage(dbFile);
+	void initializeUser(User a);
 };
