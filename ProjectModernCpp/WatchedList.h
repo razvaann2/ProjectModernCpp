c6280843ea4069b7a6d264ac8c5b83ed66aaec4d
Olteanu Razvan <razvaann2@gmail.com>
2022-12-13 22:18:11 +0200

    WatchedList header

---
+++ ProjectModernCpp/WatchedList.h
@@ -0,0 +1,14 @@
+#pragma once
+class WatchedList
+{
+private:
+	int MovieID, UserID;
+public:
+	int GetMovieID()const;
+	int GetUserID()const;
+	void SetUserID(int UserID);
+	void SetMovieID(int MovieID);
+	WatchedList(int MovieID = 0, int UserID = 0);
+	WatchedList(const WatchedList& a);
+	~WatchedList();
+};
