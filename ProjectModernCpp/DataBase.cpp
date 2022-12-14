61ecea51fd11e4f7100cdf7da7a5738bf98284aa
Horia <senduchoria@yahoo.com>
2022-12-14 22:20:42 +0200

    Movies database update

--- ProjectModernCpp/DataBase.cpp
+++ ProjectModernCpp/DataBase.cpp
@@ -51,6 +51,20 @@ void DataBase::AddMovies()
 			substr = s.substr(poz1 + 1, poz2 - poz1 - 1);
 		}
 		movieToRead.SetTitle(substr);
+		poz1 = poz2;
+		if (s[poz1 + 1] == '"')
+		{
+			poz1 = s.find('"', poz1 + 1);
+			poz2 = s.find('"', poz1 + 1);
+			substr = s.substr(poz1 + 1, poz2 - poz1 - 1);
+			poz2 = s.find(',', poz2 + 1);
+		}
+		else
+		{
+			poz2 = s.find(',', poz1 + 1);
+			substr = s.substr(poz1 + 1, poz2 - poz1 - 1);
+		}
+		movieToRead.SetDirector(substr);
 	}
 }
 
