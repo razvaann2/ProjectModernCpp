5d23134fd70226d9072b5de1792cf23c7bdf1692
CTINDragos <bogstang2000@GMAIL.COM>
2023-01-08 14:50:17 +0200

    testing more setters

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -12,12 +12,26 @@ namespace UnitTest
 	public:
 		
 	
-		TEST_METHOD(TestingSettersandGetters)
+		TEST_METHOD(TestingSetCast)
 		{
 			Movie* m = new Movie();
 			m->SetCast("Leonardo Dicaprio");
 			std::string s = "Leonardo Dicaprio";
 			Assert::AreEqual(s, m->GetCast());
+		}
+		TEST_METHOD(TestingSetCountry)
+		{
+			Movie* m = new Movie();
+			m->SetCountry("Romania");
+			std::string s = "Romania";
+			Assert::AreEqual(s, m->GetCountry());
+		}
+		TEST_METHOD(TestingSet)
+		{
+			Movie* m = new Movie();
+			m->SetDateAdded("01.01.2000");
+			std::string s = "01.01.2000";
+			Assert::AreEqual(s, m->GetDateAdded());
 		}
 	};
 }
